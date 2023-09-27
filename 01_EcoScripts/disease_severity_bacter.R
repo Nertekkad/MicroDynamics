727c186dee69283d8e8843875255403028050176
Nertekkad <61397302+Nertekkad@users.noreply.github.com>
2023-09-27 11:22:04 -0600

    Disease severity

---
+++ 01_EcoScripts/disease_severity_bacter.R
@@ -0,0 +1,322 @@
+setwd("~/EcoModel")
+# Data loading
+library(readxl)
+otu_bacter<-read.csv("1_bacteria.csv")
+meta_data<-read.csv("clin_attr_long.csv")
+
+# Severe patient's data
+severe<-meta_data$sample.number[which(meta_data$disease.severity == "severe")]
+
+severe_data<-list()
+for(i in 1:length(severe)){
+  severe_data[[i]]<-subset(otu_bacter, grepl(severe[i], PatientID))
+}
+severe_data<-do.call(rbind, severe_data)
+severe_data<-severe_data[,-1]
+
+# Moderate patient´s data
+moderate<-meta_data$sample.number[which(meta_data$disease.severity == "moderate")]
+
+moderate_data<-list()
+for(i in 1:length(moderate)){
+  moderate_data[[i]]<-subset(otu_bacter, grepl(moderate[i], PatientID))
+}
+moderate_data<-do.call(rbind, moderate_data)
+moderate_data<-moderate_data[,-1]
+
+# Mild patient´s data
+mild<-meta_data$sample.number[which(meta_data$disease.severity == "mild")]
+
+mild_data<-list()
+for(i in 1:length(mild)){
+  mild_data[[i]]<-subset(otu_bacter, grepl(mild[i], PatientID))
+}
+mild_data<-do.call(rbind, mild_data)
+mild_data<-mild_data[,-1]
+
+# Networks' inference
+
+library(minet)
+library(igraph)
+
+# Severe's network
+mim <- build.mim(severe_data, estimator="spearman")
+aracne_mat <- aracne(mim)
+severeNet<-graph.adjacency(aracne_mat)
+severeNet<-as.undirected(severeNet)
+
+# Moderate's network
+mim <- build.mim(moderate_data, estimator="spearman")
+aracne_mat <- aracne(mim)
+moderateNet<-graph.adjacency(aracne_mat)
+moderateNet<-as.undirected(moderateNet)
+
+# Mild's network
+mim <- build.mim(mild_data, estimator="spearman")
+aracne_mat <- aracne(mim)
+mildNet<-graph.adjacency(aracne_mat)
+mildNet<-as.undirected(mildNet)
+
+# muxViz object
+library(muxViz)
+bacter_ml<-list(severeNet, moderateNet, mildNet)
+
+
+# Centrality function
+ctr<-function(g.list, ctr_type){
+  require(igraph)
+  if(ctr_type == "degree"){
+    for(i in 1:length(g.list)){
+      ctr_max<-which.max(degree(g.list[[i]]))
+      ctr<-degree(g.list[[i]])/degree(g.list[[i]])[ctr_max]
+      Redpal<-heat.colors(5, alpha=1)
+      hl1<-which(ctr > 0 & ctr <= 0.2)
+      V(g.list[[i]])[hl1]$hl<-Redpal[5]
+      hl2<-which(ctr > 0.2 & ctr <= 0.4)
+      V(g.list[[i]])[hl2]$hl<-Redpal[4]
+      hl3<-which(ctr > 0.4 & ctr <= 0.6)
+      V(g.list[[i]])[hl3]$hl<-Redpal[3]
+      hl4<-which(ctr > 0.6 & ctr <= 0.8)
+      V(g.list[[i]])[hl4]$hl<-Redpal[2]
+      hl5<-which(ctr > 0.8 & ctr <= 1)
+      V(g.list[[i]])[hl5]$hl<-Redpal[1]
+      vertex.attributes(g.list[[i]])$hl
+    }
+  }
+  if(ctr_type == "betweenness"){
+    for(i in 1:length(g.list)){
+      ctr_max<-which.max(betweenness(g.list[[i]]))
+      ctr<-betweenness(g.list[[i]])/betweenness(g.list[[i]])[ctr_max]
+      Redpal<-heat.colors(5, alpha=1)
+      hl1<-which(ctr > 0 & ctr <= 0.2)
+      V(g.list[[i]])[hl1]$hl<-Redpal[5]
+      hl2<-which(ctr > 0.2 & ctr <= 0.4)
+      V(g.list[[i]])[hl2]$hl<-Redpal[4]
+      hl3<-which(ctr > 0.4 & ctr <= 0.6)
+      V(g.list[[i]])[hl3]$hl<-Redpal[3]
+      hl4<-which(ctr > 0.6 & ctr <= 0.8)
+      V(g.list[[i]])[hl4]$hl<-Redpal[2]
+      hl5<-which(ctr > 0.8 & ctr <= 1)
+      V(g.list[[i]])[hl5]$hl<-Redpal[1]
+      vertex.attributes(g.list[[i]])$hl
+    }
+  }
+  if(ctr_type == "closeness"){
+    for(i in 1:length(g.list)){
+      ctr_max<-which.max(closeness(g.list[[i]]))
+      ctr<-closeness(g.list[[i]])/closeness(g.list[[i]])[ctr_max]
+      Redpal<-heat.colors(5, alpha=1)
+      hl1<-which(ctr > 0 & ctr <= 0.2)
+      V(g.list[[i]])[hl1]$hl<-Redpal[5]
+      hl2<-which(ctr > 0.2 & ctr <= 0.4)
+      V(g.list[[i]])[hl2]$hl<-Redpal[4]
+      hl3<-which(ctr > 0.4 & ctr <= 0.6)
+      V(g.list[[i]])[hl3]$hl<-Redpal[3]
+      hl4<-which(ctr > 0.6 & ctr <= 0.8)
+      V(g.list[[i]])[hl4]$hl<-Redpal[2]
+      hl5<-which(ctr > 0.8 & ctr <= 1)
+      V(g.list[[i]])[hl5]$hl<-Redpal[1]
+      vertex.attributes(g.list[[i]])$hl
+    }
+  }
+  return(g.list)
+}
+
+bacter_ml<-ctr(bacter_ml, "degree")
+
+# Degree´s node-color matrix
+colmat<-matrix(c(V(bacter_ml[[1]])$hl,V(bacter_ml[[2]])$hl,V(bacter_ml[[3]])$hl),
+               nrow = length(V(bacter_ml[[1]])), ncol = length(bacter_ml))
+
+absmat<-matrix(c(log(colSums(severe_data)+2)/20,log(colSums(moderate_data)+2)/20,
+                 log(colSums(mild_data)+2)/20), nrow = length(V(bacter_ml[[1]])),
+                 ncol = length(bacter_ml))
+
+# Degree 3D plot
+lay <- layoutMultiplex(bacter_ml, layout="fr", ggplot.format=F, box=T)
+plot_multiplex3D(bacter_ml, layer.layout=lay,
+                 layer.colors=rainbow(length(bacter_ml)),
+                 layer.shift.x=0.5, layer.space=2,
+                 layer.labels=c("Severe", "Moderate", "Mild"), layer.labels.cex=1.5,
+                 node.size.values="auto", node.size.scale=absmat,
+                 node.colors=colmat,
+                 edge.colors="black",
+                 node.colors.aggr=NULL,
+                 show.aggregate=F)
+
+
+# Networks' connectivity analysis
+
+degree_df <- data.frame(Species = vertex.attributes(bacter_ml[[1]])$name,
+                        Severe = degree(bacter_ml[[1]]),
+                        Moderate = degree(bacter_ml[[2]]),
+                        Mild = degree(bacter_ml[[3]]))
+
+degree_df<-degree_df[-which(rowSums(degree_df[,c(2,3,4)])/
+                              mean(rowSums(degree_df[,c(2,3,4)]))<0.9),]
+
+# Plot of degree between layers
+
+library(gridExtra)
+p1<-ggplot(data=degree_df, aes(x=Species, y=Severe, fill=NULL)) +
+  geom_bar(stat="identity")
+p2<-ggplot(data=degree_df, aes(x=Species, y=Moderate, fill=NULL)) +
+  geom_bar(stat="identity")
+p3<-ggplot(data=degree_df, aes(x=Species, y=Mild, fill=NULL)) +
+  geom_bar(stat="identity")
+
+grid.arrange(p1 + coord_flip() +
+               theme(axis.text.y = element_text(size = 0.01),
+                     legend.position = "none") + ylab("Severe"),
+             p2 + coord_flip() +
+               theme(axis.text.y = element_text(size = 0.01),
+                     legend.position = "none") + ylab("Moderate"),
+             p3 + coord_flip() +
+               theme(axis.text.y = element_text(size = 0.01),
+                     legend.position = "none") + ylab("Mild"),
+             ncol=3)
+
+
+# Diversity analysis
+
+# Shannon diversity
+Div_Shannon <- function(abundancias_ab){
+  abs_rel <- abundancias_ab/sum(abundancias_ab)
+  Shannon <- -sum(abs_rel*log(abs_rel))
+  return(Shannon)
+}
+
+# Simpson dominance
+Dom_Simpson <- function(abundancias_ab){
+  abs_rel <- abundancias_ab/sum(abundancias_ab)
+  Simpson <- sum(abs_rel^2)
+  return(Simpson)
+}
+
+# Pielou evenness
+Eq_Pielou <- function(abundancias_ab){
+  abs_rel <- abundancias_ab/sum(abundancias_ab)
+  Shannon <- -sum(abs_rel*log(abs_rel))
+  Pielou <- Shannon/log(length(abundancias_ab))
+  return(Pielou)
+}
+
+# Abundance-table diversity function
+ab_table_div<-function(ab_table, diversity_type){
+  require(vegan)
+  if(diversity_type == "shannon"){
+    div_table<-c()
+    for(i in 1:ncol(ab_table)){
+      div_table[i]<-diversity(ab_table[, i])
+    }
+  } else
+    if(diversity_type == "simpson"){
+      div_table<-c()
+      for(i in 1:ncol(ab_table)){
+        div_table[i]<-Dom_Simpson(ab_table[, i])
+      }
+    } else
+      if(diversity_type == "pielou"){
+        div_table<-c()
+        for(i in 1:ncol(ab_table)){
+          S <- specnumber(ab_table[, i])
+          div_table[i] <- diversity(ab_table[, i])/log(S)
+        }
+      } else
+        if(diversity_type == "ginisimpson"){
+          div_table<-c()
+          for(i in 1:ncol(ab_table)){
+            div_table[i]<-1-Dom_Simpson(ab_table[, i])
+          }
+        }
+  return(div_table)
+}
+
+# Dominance between patients
+dom_severe<-ab_table_div(t(severe_data), "simpson")
+dom_severe<-data.frame("Severity" = rep("Severe", length(dom_severe)),
+                   "Data" = dom_severe)
+dom_moderate<-ab_table_div(t(moderate_data), "simpson")
+dom_moderate<-data.frame("Severity" = rep("Moderate", length(dom_moderate)),
+                       "Data" = dom_moderate)
+dom_mild<-ab_table_div(t(mild_data), "simpson")
+dom_mild<-data.frame("Severity" = rep("Mild", length(dom_mild)),
+                         "Data" = dom_mild)
+dom_severity<-rbind(dom_severe, dom_moderate, dom_mild)
+
+# Violin plot
+library(ggplot2)
+ggplot(dom_severity, aes(x=dom_severity$Severity, y=dom_severity$Data)) +
+  geom_violin(fill="darkslategray3", color="black") +
+  geom_boxplot(width=0.15, notch=TRUE,
+               fill=c("green3", "darkorange1", "red3"),
+               color="black") + ggtitle("Simpson dominance") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  xlab("") + ylab("")
+
+
+# Rank-abundance analysis
+
+library(RADanalysis)
+sample_classes <- c(rep(1, nrow(dom_severe)),rep(2, nrow(dom_moderate)),
+                    rep(3, nrow(dom_mild)))
+line_cols <- c("red3","darkorange1","green3")
+
+severity_mat<-rbind(severe_data, moderate_data, mild_data)
+
+# Normalized abundances
+n_severity_mat<-severity_mat/rowSums(severity_mat)
+
+# Sort the abundances in a decreasing order
+sorted_abs<-list()
+for(i in 1:nrow(n_severity_mat)){
+  sorted_abs[[i]]<-sort(n_severity_mat[i,], decreasing = T)
+}
+n_severity_mat<-matrix(unlist(sorted_abs), nrow=length(sorted_abs),
+                   ncol=length(sorted_abs[[1]]), byrow=TRUE)
+
+# Plot the axis
+plot(1e10,xlim = c(1,15),ylim = c(0,0.8),
+     xlab = "Species rank",ylab = "Abundance",cex.lab = 1.5,axes = FALSE)
+sfsmisc::eaxis(side = 1,at = c(1,5,10,15))
+sfsmisc::eaxis(side = 2,at = c(0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8),
+               las = 0)
+
+# Rank-abundance colors per disease severity
+a <- representative_RAD(norm_rad = n_severity_mat, sample_ids = which(sample_classes == 1),
+                        plot = TRUE,confidence = 0.9,with_conf = TRUE,
+                        col = scales::alpha(line_cols[1],0.5),border = NA)
+a <- representative_RAD(norm_rad = n_severity_mat,sample_ids = which(sample_classes == 2),
+                        plot = TRUE,confidence = 0.9,with_conf = TRUE,
+                        col = scales::alpha(line_cols[2],0.5),border = NA)
+a <- representative_RAD(norm_rad = n_severity_mat,sample_ids = which(sample_classes == 3),
+                        plot = TRUE,confidence = 0.9,with_conf = TRUE,
+                        col = scales::alpha(line_cols[3],0.5),border = NA)
+legend("topright",bty = "n",
+       legend = c("Severe","Moderate", "Mild"),
+       col = line_cols, lwd = 3)
+
+
+# MSD analysis
+
+d <- dist(x = n_severity_mat,method = "manhattan")
+mds <- cmdscale(d = d,k = 5,eig = TRUE)
+plot(mds$points,xlab = "First coordinate",ylab = "Second coordinate",pch = 19,
+     cex =1,col = line_cols[sample_classes],
+     main = "MDS plot with representative points \n of each group and error bars")
+a <- representative_point(input = mds$points,ids = which(sample_classes == 1),
+                          col = scales::alpha(line_cols[1],0.5),
+                          plot = TRUE,standard_error_mean = TRUE,pch = 19, cex = 4)
+a <- representative_point(input = mds$points,ids = which(sample_classes == 2),
+                          col = scales::alpha(line_cols[2],0.5),
+                          plot = TRUE,standard_error_mean = TRUE,pch = 19, cex = 4)
+a <- representative_point(input = mds$points,ids = which(sample_classes == 3),
+                          col = scales::alpha(line_cols[3],0.5),
+                          plot = TRUE,standard_error_mean = TRUE,pch = 19, cex = 4)
+legend("topright",bty = "n",legend = c("Severe","Moderate","Mild"),
+       col = line_cols,pch = 19)
+
+
+
+             
+
